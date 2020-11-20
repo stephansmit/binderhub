cc7e28f559c8e10c6284f56129b097f7a31fa948
stephansmit <stephansmit@hotmail.com>
2020-11-20 16:17:45 +0100

    Update deploy_cluster.sh

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -1,1 +1,12 @@
-az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
+VNET_ID=$(az network vnet show \
+    --resource-group binderhub \
+    --name binderhub_vnet \
+    --query id \
+    --output tsv)
+    
+SUBNET_ID=$(az network vnet subnet show \
+    --resource-group binderhub \
+    --vnet-name binderhub_vnet \
+    --name binderhub_subnet \
+    --query id \
+    --output tsv)
