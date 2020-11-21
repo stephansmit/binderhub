fdef19e0535a911a47be9b8baf3cdeab9acfb7a3
stephansmit <stephansmit@hotmail.com>
2020-11-21 17:29:58 +0100

    Create deploy_jupyterhub.sh

---
+++ deploy_jupyterhub.sh
@@ -0,0 +1,14 @@
+RANDOM_HEX=$(openssl rand -hex 32)
+printf "proxy:\n\tsecretToken: \"$RANDOM_HEX\"" > config.yaml
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
+
+RELEASE=jhub
+NAMESPACE=jhub
+
+helm upgrade --cleanup-on-fail \
+  --install $RELEASE jupyterhub/jupyterhub \
+  --namespace $NAMESPACE \
+  --create-namespace \
+  --version=0.9.0 \
+  --values config.yaml
